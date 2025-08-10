ed859f4350f505cdfcb8096fd51dcb83d3371a64
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-10 19:35:25 +0500

    Workflow added v4

--- backend/test/example_test.js
+++ backend/test/example_test.js
@@ -7,7 +7,7 @@ const connectDB = require('../config/db');
 const mongoose = require('mongoose');
 const sinon = require('sinon');
 const Task = require('../models/Task');
-const { updateTask,getTasks,addTask,deleteTask } = require('../controllers/taskControllerr');
+const { updateTask,getTasks,addTask,deleteTask } = require('./controllers/taskController');
 const { expect } = chai;
 
 chai.use(chaiHttp);
