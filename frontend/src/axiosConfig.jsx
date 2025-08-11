84591c5536c00bad8e6a14d5f90742c08ab49ca7
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-11 09:54:15 +0500

    workflow added v17

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.104.79.25:5001', // local
+  baseURL: 'http://3.104.79.2:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
