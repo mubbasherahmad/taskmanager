97a853d995674fa5a9d6e2f6fed1ff8b0ae45830
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-08-14 09:51:45 +0500

    workflow added v19

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-// Add this line with your other route imports
-app.use('/api/sensors', require('./routes/sensorRoutes'));
-//app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Export the app object for testing
 if (require.main === module) {
